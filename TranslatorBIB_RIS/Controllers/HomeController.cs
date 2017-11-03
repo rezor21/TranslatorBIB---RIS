e5fcf6659149fdbd2db148c081298790f6a04654
lodanielo <daniel18186@wp.pl>
2017-11-03 18:51:35 +0100

    Przykładowe pobieranie plików
    2 metody w controlerze Home i pare linijek w views/home/index

--- TranslatorBIB_RIS/Controllers/HomeController.cs
+++ TranslatorBIB_RIS/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -21,6 +22,39 @@ namespace TranslatorBIB_RIS.Controllers
             ViewBag.Title = "Angular Page";
 
             return View("Angular");
+        }
+        private void showFile(HttpPostedFileBase file)
+        {
+            BinaryReader b = new BinaryReader(file.InputStream);
+            byte[] binData = b.ReadBytes(file.ContentLength);
+            string result = System.Text.Encoding.UTF8.GetString(binData);
+
+
+            ViewData["Text"] = result;
+
+        }
+
+        [System.Web.Mvc.HttpPost]
+        public ActionResult Index(HttpPostedFileBase file)
+        {
+
+            if (file != null && file.ContentLength > 0)
+                try
+                {
+
+                    showFile(file);
+                    ViewBag.Message = "File uploaded successfully ";
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.Message = "ERROR:" + ex.Message.ToString();
+                }
+            else
+            {
+                ViewBag.Message = "You have not specified a file.";
+            }
+
+            return View();
         }
     }
 }
